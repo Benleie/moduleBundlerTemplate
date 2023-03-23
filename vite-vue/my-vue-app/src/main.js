d86e7241ff4af5bfff7b8b94cc429e7b680fa0b6
李奔 <liben@leyopharm.com>
2023-03-23 11:11:40 +0800

    test: 测试vite

--- vite-vue/my-vue-app/src/main.js
+++ vite-vue/my-vue-app/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+let hh = 'hh';
+console.log(hh)
+console.log(hh, ['f', 'ff'].includes('ffff'))
 createApp(App).mount('#app')
