dfaa2552a24603298b8ec7bdbe654c95eab92e90
李奔 <liben@leyopharm.com>
2023-03-23 11:31:11 +0800

    test(done): vue-cli 可以兼容firefox38

--- vue3-vuecli/src/main.js
+++ vue3-vuecli/src/main.js
@@ -2,5 +2,8 @@ import 'core-js'
 import { createApp } from 'vue'
 import App from './App.vue'
 console.log(['main', 'dd'].includes('main'))
-
+console.log(['index', 'dd'].includes('html'))
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+const obj = {}
+console.log(obj?.foo?.hhh)
 createApp(App).mount('#app')
