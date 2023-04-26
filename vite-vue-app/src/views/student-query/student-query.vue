4524af038c6081cc822c54f6c26c80c308c2eb83
李奔 <liben@leyopharm.com>
2023-04-26 19:01:21 +0800

    feat: vite table页 为tableData添加临时属性

--- vite-vue-app/src/views/student-query/student-query.vue
+++ vite-vue-app/src/views/student-query/student-query.vue
@@ -7,20 +7,37 @@ defineProps({
 
 // TODO: 与reactive的区别
 const listData = ref([])
+const tableLoading = ref(false)
 
 const handleSearch = async () => {
+  tableLoading.value = true
   const data = await getStudentData()
-  console.log(data)
+  tableLoading.value = false
+  // 无需提前添加exportLoading属性
+  // data.forEach(item => { item.exportLoading = false })
   listData.value = data
+}
+const handleExport = (scope) => {
+  scope.row.exportLoading = true
+  setTimeout(() => {
+    scope.row.exportLoading = false
+  }, 1000)
 }
 </script>
 
 <template>
-  <el-button @click="handleSearch">搜索</el-button>
+  <el-button :disabled="tableLoading" @click="handleSearch">搜索</el-button>
   <el-table :data="listData" border style="width: 100%;margin-top:20px;">
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" min-width="180" />
+    <el-table-column label="操作" min-width="180" >
+      <template #default="scope">
+        <el-button :disabled="scope.row.exportLoading" size="small" @click="handleExport(scope)"
+          >Export</el-button
+        >
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
