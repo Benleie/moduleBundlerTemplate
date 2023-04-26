b00635077205322a6d858ea377c5becee02ea0fd
李奔 <liben@leyopharm.com>
2023-04-26 15:56:35 +0800

    feat: vite 添加自定义页面

--- vite-vue-app/src/App.vue
+++ vite-vue-app/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
