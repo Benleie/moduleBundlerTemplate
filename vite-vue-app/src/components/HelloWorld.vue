f762b4e7223685e33ae773c4a71f555e2c4efd40
李奔 <liben@leyopharm.com>
2023-04-26 16:06:32 +0800

    refacor: vite hello-world改造

--- vite-vue-app/src/components/HelloWorld.vue
+++ vite-vue-app/src/components/HelloWorld.vue
@@ -9,6 +9,14 @@ const count = ref(0)
 </script>
 
 <template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
   <h1>{{ msg }}</h1>
 
   <div class="card">
@@ -37,5 +45,17 @@ const count = ref(0)
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
