b4904cea3cd8bcf71feec8f93deff3add93a269e
李奔 <liben@leyopharm.com>
2023-04-27 11:16:50 +0800

    feat: vite 引入echarts

--- vite-vue-app/src/router/route.js
+++ vite-vue-app/src/router/route.js
@@ -1,9 +1,11 @@
 import HelloWorld from '../components/HelloWorld.vue'
 import StudentQuery from '../views/student-query/student-query.vue';
+import Chart from '../views/charts/first.vue';
 const About = { template: '<div>About</div>' }
 
 export const pageRoutes = [
-  { path: '/', component: StudentQuery },
+  { path: '/', component: Chart },
+  { path: '/table', component: StudentQuery },
   { path: '/hello', component: HelloWorld },
   { path: '/about', component: About },
 ]
