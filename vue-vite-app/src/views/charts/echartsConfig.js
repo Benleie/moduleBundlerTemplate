aeb2af9962abed087ee7ddb4d71a533995982a0e
李奔 <liben@leyopharm.com>
2023-05-10 17:33:16 +0800

    echarts: bar series数据填充

--- vue-vite-app/src/views/charts/echartsConfig.js
+++ vue-vite-app/src/views/charts/echartsConfig.js
@@ -1,17 +1,48 @@
 import * as echarts from 'echarts';
 
 const options = {
+  color: ['#2F54EB', '#722ED1', '#EB2F96', '#52C41A', '#fff'],
   title: {
-    text: 'ECharts 入门示例'
+    // text: 'ECharts 入门示例'
   },
-  tooltip: {},
+  legend: {
+    // orient: 'vertical',
+    orient: 'horizontal',
+    left: 0,
+    top: 0
+  },
+  tooltip: {
+    order: 'valueDesc',
+    trigger: 'axis',
+    axisPointer: {
+      type: "shadow"
+    }
+  },
   xAxis: {
+    type: 'category',
     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
   },
-  yAxis: {},
+  yAxis: {
+    name: '单位（元）'
+  },
   series: [
     {
-      name: '销量',
+      name: '数据一',
+      type: 'bar',
+      data: [20, 20, 36, 10, 10, 20]
+    },
+    {
+      name: '数据二',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    },
+    {
+      name: '数据三',
+      type: 'bar',
+      data: [30, 20, 36, 10, 10, 20]
+    },
+    {
+      name: '数据四',
       type: 'bar',
       data: [5, 20, 36, 10, 10, 20]
     }
